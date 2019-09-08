a759aac6c80efdc3a5f7bd6d1aa141bc0593dbe8
Christopher Denker <mail@chrisden.de>
2019-09-08 22:20:12 +0200

    Bugfixes and player collision implementation, separation of Input from Player

--- Snake.cpp
+++ Snake.cpp
@@ -3,16 +3,18 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <vector>
 
 // Constants
 const char *GAME_TITLE = "Snake";
-const unsigned int SCREEN_WIDTH = 1024;
-const unsigned int SCREEN_HEIGHT = 768;
-const unsigned int BPART_WIDTH = 50;
-const unsigned int BPART_HEIGHT = 50;
-const unsigned int FOOD_WIDTH = 50;
-const unsigned int FOOD_HEIGHT = 50;
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+const unsigned int SCALE = 20;
+const unsigned int BPART_WIDTH = SCREEN_WIDTH / SCALE;
+const unsigned int BPART_HEIGHT = SCREEN_HEIGHT / SCALE;
+const unsigned int FOOD_WIDTH = SCREEN_WIDTH / SCALE;
+const unsigned int FOOD_HEIGHT = SCREEN_HEIGHT / SCALE;
 
 class Screen {
 public:
@@ -57,136 +59,44 @@ public:
 	unsigned int width;
 	unsigned int height;
 
-	Food(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
+	Food();
 
+	void spawn();
 	void render(Screen &screen);
-	void update(unsigned int screen_width, unsigned int screen_height);
 };
 
 class Player {
 public:
 	Player(unsigned int head_x, unsigned int head_y, unsigned int bpart_width, unsigned int bpart_height);
 
+	void reinit(unsigned int head_x, unsigned int head_y, unsigned int bpart_width, unsigned int bpart_height);
 	void render(Screen &screen);
-	void update(unsigned int screen_width, unsigned int screen_height);
-	void reset_inputs();
+	void update(Input &input, unsigned int screen_width, unsigned int screen_height);
 	void grow();
+	bool head_collided_with(Food &f);
+	bool collided_with_himself();
+	inline void die() { dead = true; }
 
-	inline Input & get_input_ref() { return input; }
 	inline BodyPart & get_head_ref() { return bparts[0]; }
+	inline unsigned int get_size() { return bparts.size(); }
 	inline unsigned int get_total_width() { return bparts.size() * bparts[0].width; }
 	inline unsigned int get_total_height() { return bparts.size() * bparts[0].height; }
-
-	inline bool head_collided_with(Food &f)
-	{
-		// TODO Check if this is correct
-		return (bparts[0].x == f.x && (bparts[0].y > f.y + f.height || bparts[0].y + bparts[0].height > f.y))
-			|| (bparts[0].y == f.y && (bparts[0].x < f.x + f.width  || bparts[0].x + bparts[0].height > f.x));
-	}
+	inline bool is_dead() { return dead; }
 
 private:
 	std::vector<BodyPart> bparts;
 	int direction_x;
 	int direction_y;
-	Input input;
+	bool dead;
 };
 
-Player::Player(unsigned int head_x, unsigned int head_y, unsigned int bpart_width, unsigned int bpart_height)
-:  bparts{ { head_x, head_y, head_x, head_y, bpart_width, bpart_height }, { head_x, head_y + bpart_width, head_x, head_y + bpart_width, bpart_width, bpart_height } },
-  direction_x(0),
-  direction_y(-1)
-{
-	reset_inputs();
-}
-
-void Player::render(Screen &screen)
-{
-	// Render all body parts as blue rectangles
-	for (BodyPart &bp : bparts) {
-		screen.render_rect(bp.x, bp.y, bp.width, bp.height, 0, 0, 255);
-	}
-}
-
-void Player::reset_inputs()
+void reset_inputs(Input &input)
 {
 	input.left = false;
 	input.right = false;
 	input.up = false;
 	input.down = false;
 	input.restart = false;
-}
-
-void Player::grow()
-{
-	BodyPart &old_tail = bparts.back();
-	// TODO Also consider special cases (where snake leaves the screen)
-	std::puts("Player::grow");
-}
-
-void Player::update(unsigned int screen_width, unsigned int screen_height)
-{
-	// Set direction according to input
-	Input &input = get_input_ref();
-	if (input.up && direction_y != 1) {
-		direction_x = 0;
-		direction_y = -1;
-	} else if (input.down && direction_y != -1) {
-		direction_x = 0;
-		direction_y = 1;
-	} else if (input.left && direction_x != 1) {
-		direction_x = -1;
-		direction_y = 0;
-	} else if (input.right && direction_x != -1) {
-		direction_x = 1;
-		direction_y = 0;
-	}
-	if (input.restart) {
-		// TODO Reinitialize
-	}
-	reset_inputs();
-
-	// Update the head's position
-	BodyPart &head = get_head_ref();
-	head.x = head.next_x;
-	head.y = head.next_y;
-	head.next_x = (head.x + direction_x * head.width) % screen_width;
-	int head_next_x = head.x + direction_x * head.width;
-	int head_next_y = head.y + direction_y * head.height;
-	if (head_next_x < 0) {	// TODO Integer overflow -> undefined behavior?
-		head.next_x = screen_width - get_total_width();
-	} else {
-		head.next_x = head_next_x % screen_width;
-	}
-	if (head_next_y < 0) {	// TODO Integer overflow -> undefined behavior?
-		head.next_y = screen_height - get_total_height();
-	} else {
-		head.next_y = head_next_y % screen_height;
-	}
-	
-	// Update the other body parts' positions
-	for (unsigned int i = 1; i < bparts.size(); i++) {
-		bparts[i].x = bparts[i].next_x;
-		bparts[i].y = bparts[i].next_y;
-		bparts[i].next_x = bparts[i - 1].x;
-		bparts[i].next_y = bparts[i - 1].y;	
-	}
-}
-
-Food::Food(unsigned int x, unsigned int y, unsigned int width, unsigned int height)
-: x(x),
-  y(y),
-  width(width),
-  height(height)
-{}
-
-void Food::render(Screen &screen)
-{
-	screen.render_rect(x, y, width, height, 255, 0, 0); // render food as red rect
-}
-
-void Food::update(unsigned int screen_width, unsigned int screen_height)
-{
-	// TODO
 }
 
 void process_input(Input &input, bool &game_running)
@@ -221,6 +131,137 @@ void process_input(Input &input, bool &game_running)
 		break;
 	}
 }
+
+Player::Player(unsigned int head_x, unsigned int head_y, unsigned int bpart_width, unsigned int bpart_height)
+: bparts{ { head_x, head_y, head_x, head_y, bpart_width, bpart_height }, { head_x, head_y + bpart_height, head_x, head_y + bpart_height, bpart_width, bpart_height } },
+  direction_x(0),
+  direction_y(-1),
+  dead(false)
+{}
+
+void Player::reinit(unsigned int head_x, unsigned int head_y, unsigned int bpart_width, unsigned int bpart_height)
+{
+	bparts.clear();
+	BodyPart head = { head_x, head_y, head_x, head_y, bpart_width, bpart_height };
+	bparts.push_back(head);
+	BodyPart tail = { head_x, head_y + bpart_height, head_x, head_y + bpart_height, bpart_width, bpart_height };
+	bparts.push_back(tail);
+	direction_x = 0;
+	direction_y = -1;
+	dead = false;
+}
+
+void Player::render(Screen &screen)
+{
+	// Render head as black rectangle
+	BodyPart &head = bparts[0];
+	screen.render_rect(head.x, head.y, head.width, head.height, 0, 0, 0);
+	for (unsigned int i = 1; i < bparts.size(); i++) {
+		BodyPart &bp = bparts[i];
+		if (!dead) {
+			screen.render_rect(bp.x, bp.y, bp.width, bp.height, 0, 0, 255);
+		} else {
+			screen.render_rect(bp.x, bp.y, bp.width, bp.height, 0, 0, 0);
+		}
+	}
+}
+
+void Player::grow()
+{
+	BodyPart &old_tail = bparts.back();
+	BodyPart new_tail = { old_tail.x, old_tail.y, old_tail.x, old_tail.y, old_tail.width, old_tail.height };
+	bparts.push_back(new_tail);
+}
+
+bool Player::head_collided_with(Food &f)
+{
+	// Taken from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+	return bparts[0].x < f.x + f.width && bparts[0].x + bparts[0].width > f.x
+		&& bparts[0].y < f.y + f.height && bparts[0].y + bparts[0].height > f.y;
+}
+
+// TODO Check if this can be done more efficiently
+bool Player::collided_with_himself()
+{
+	// Check for head colliding with any body part
+	BodyPart &head = bparts[0];
+	for (unsigned int i = 1; i < bparts.size(); i++) {
+		BodyPart &bp = bparts[i];
+		// Check if head collided with bp
+		// Taken from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+		if (head.x < bp.x + bp.width && head.x + head.width > bp.x
+			&& head.y < bp.y + bp.height && head.y + head.height > bp.y)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void Player::update(Input &input, unsigned int screen_width, unsigned int screen_height)
+{
+	// Set direction according to input
+	if (input.up && direction_y != 1) {
+		direction_x = 0;
+		direction_y = -1;
+	} else if (input.down && direction_y != -1) {
+		direction_x = 0;
+		direction_y = 1;
+	} else if (input.left && direction_x != 1) {
+		direction_x = -1;
+		direction_y = 0;
+	} else if (input.right && direction_x != -1) {
+		direction_x = 1;
+		direction_y = 0;
+	}
+	reset_inputs(input);
+
+	// Update the head's position
+	// TODO Make sure it is divisible by BPART_WIDTH and BPART_HEIGHT
+	BodyPart &head = get_head_ref();
+	head.x = head.next_x;
+	head.y = head.next_y;
+	head.next_x = (head.x + direction_x * head.width) % screen_width;
+	int head_next_x = head.x + direction_x * head.width;
+	int head_next_y = head.y + direction_y * head.height;
+	if (head_next_x < 0) {
+		head.next_x = screen_width;
+	} else {
+		head.next_x = head_next_x % screen_width;
+	}
+	if (head_next_y < 0) {
+		head.next_y = screen_height;
+	} else {
+		head.next_y = head_next_y % screen_height;
+	}
+	
+	// Update the other body parts' positions
+	for (unsigned int i = 1; i < bparts.size(); i++) {
+		bparts[i].x = bparts[i].next_x;
+		bparts[i].y = bparts[i].next_y;
+		bparts[i].next_x = bparts[i - 1].x;
+		bparts[i].next_y = bparts[i - 1].y;	
+	}
+}
+
+Food::Food()
+{
+	spawn();
+}
+
+void Food::spawn()
+{
+	x = (std::rand() % SCALE) * FOOD_WIDTH;
+	y = (std::rand() % SCALE) * FOOD_HEIGHT;
+	width = FOOD_WIDTH;
+	height = FOOD_HEIGHT;
+}
+
+void Food::render(Screen &screen)
+{
+	screen.render_rect(x, y, width, height, 255, 0, 0); // render food as red rect
+}
+
 
 Screen::Screen(unsigned int width, unsigned int height)
 : width(width),
@@ -272,46 +313,57 @@ void Screen::render_rect(int x, int y, int width, int height, int red, int green
 	SDL_RenderFillRect(renderer, &rect);
 }
 
-void update(Player &player, Food &food, unsigned int screen_width, unsigned int screen_height)
+void update(Player &player, Food &food, Input &input, unsigned int screen_width, unsigned int screen_height)
 {
-	player.update(screen_width, screen_height);
-	food.update(screen_width, screen_height);
+	if (input.restart) {
+		// TODO Make sure it is divisible by BPART_WIDTH and BPART_HEIGHT
+		unsigned int player_x = std::fabs(screen_width / 2);
+		unsigned int player_y = screen_height - std::fabs(screen_height / 4);
+		player.reinit(player_x, player_y, BPART_WIDTH, BPART_HEIGHT);
+		reset_inputs(input);
+	}
+	if (player.is_dead()) {
+		return;
+	}
+
+	player.update(input, screen_width, screen_height);
 
 	if (player.head_collided_with(food)) {
-		std::puts("collision");
-		// TODO
-		// Increase length of player
 		player.grow();
-		// Reset food
-//		food.reset();
-		food = Food(rand() % (SCREEN_WIDTH - FOOD_WIDTH), rand() % (SCREEN_HEIGHT - FOOD_HEIGHT), FOOD_WIDTH, FOOD_HEIGHT); // TODO Does this work? If so, why? food is a reference...
+		food.spawn();
+	}
+
+	if (player.collided_with_himself()) {
+		player.die();
+		std::printf("Final length: %u\n", player.get_size());
 	}
 }
 
 int main()
 {
 	Screen screen(SCREEN_WIDTH, SCREEN_HEIGHT);
+	Input input;
+	reset_inputs(input);
 
-	unsigned int player_x = screen.get_width() / 2;
-	unsigned int player_y = screen.get_height() - screen.get_height() / 4;
+	unsigned int player_x = std::fabs(screen.get_width() / 2);
+	unsigned int player_y = screen.get_height() - std::fabs(screen.get_height() / 4);
 	Player player(player_x, player_y, BPART_WIDTH, BPART_HEIGHT);
 
 	std::srand(std::time(nullptr));
-//	Food food(rand() % (SCREEN_WIDTH - FOOD_WIDTH), rand() % (SCREEN_HEIGHT - FOOD_HEIGHT), FOOD_WIDTH, FOOD_HEIGHT);
-	Food food(0,0, FOOD_WIDTH, FOOD_HEIGHT);
+	Food food;
 
 	bool game_running = true;
 	while (game_running) {
-		process_input(player.get_input_ref(), game_running);
+		process_input(input, game_running);
 
 		screen.clear(0, 200, 0);
 		player.render(screen);
 		food.render(screen);
 
-		update(player, food, screen.get_width(), screen.get_height());
+		update(player, food, input, screen.get_width(), screen.get_height());
 
 		screen.draw();
-		SDL_Delay(50);	// TODO Add timing logic
+		SDL_Delay(100);	// TODO Add timing logic
 	}
 	return 0;
 }
